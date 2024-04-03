5ba013c5b08a12a4470c70c551364cf642ea1265
Your Name <you@example.com>
2024-04-03 14:38:25 -0300

    Actualizaciones en la app, se hicieron cambios en users_view y se creo la pagina chat_message, la cual incluye diseño,y funcionalidades a la hora de enviar mensajes

--- lib/views/chat_view.dart
+++ lib/views/chat_view.dart
@@ -1,14 +1,156 @@
+import 'dart:io';
+
+import 'package:chat_app/widgets/chat_message.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> with TickerProviderStateMixin {
+  final _textController = TextEditingController();
+  final _focusNode = FocusNode();
+
+  final List<ChatMessage> _messages = [];
+
+  bool _isWriting = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Chat Page'),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Column(
+          children: [
+            CircleAvatar(
+              backgroundColor: Colors.blue[100],
+              maxRadius: 14,
+              child: const Text('Te', style: TextStyle(fontSize: 12)),
+            ),
+            const SizedBox(height: 3),
+            const Text(
+              'Melissa Flores',
+              style: TextStyle(color: Colors.black87, fontSize: 12),
+            )
+          ],
+        ),
+        centerTitle: true,
+        elevation: 1,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Flexible(
+              child: ListView.builder(
+                itemCount: _messages.length,
+                itemBuilder: (_, i) => _messages[i],
+                physics: const BouncingScrollPhysics(),
+                reverse: true,
+              ),
+            ),
+            const Divider(height: 1),
+            //TODO caja de texto
+            Container(
+              color: Colors.white,
+              child: _inputChat(),
+            )
+          ],
+        ),
       ),
     );
+  }
+
+  Widget _inputChat() {
+    return SafeArea(
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmit,
+                onChanged: (text) {
+                  setState(() {
+                    if (text.trim().isNotEmpty) {
+                      _isWriting = true;
+                    } else {
+                      _isWriting = false;
+                    }
+                  });
+                },
+                decoration: const InputDecoration.collapsed(
+                  hintText: 'Enviar mensaje',
+                ),
+                focusNode: _focusNode,
+              ),
+            ),
+            //Boton de enviar
+            Container(
+                margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                child: Platform.isIOS
+                    ? CupertinoButton(
+                        onPressed: _isWriting
+                            ? () => _handleSubmit(_textController.text.trim())
+                            : null,
+                        child: const Text('Enviar'),
+                      )
+                    : Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                        child: IconTheme(
+                          data: IconThemeData(color: Colors.blue[400]),
+                          child: IconButton(
+                            highlightColor: Colors.transparent,
+                            splashColor: Colors.transparent,
+                            icon: const Icon(Icons.send),
+                            onPressed: _isWriting
+                                ? () =>
+                                    _handleSubmit(_textController.text.trim())
+                                : null,
+                          ),
+                        ),
+                      ))
+          ],
+        ),
+      ),
+    );
+  }
+
+  _handleSubmit(String text) {
+    if (text.isEmpty) return;
+
+    _textController.clear();
+    _focusNode.requestFocus();
+
+    final newMessage = ChatMessage(
+      uid: '123',
+      text: text,
+      animationController: AnimationController(
+        vsync: this,
+        duration: const Duration(milliseconds: 200),
+      ),
+    );
+
+    _messages.insert(0, newMessage);
+    newMessage.animationController.forward();
+
+    setState(() {
+      _isWriting = false;
+    });
+  }
+
+  @override
+  void dispose() {
+    //TODO: ofDelSocket
+
+    for (ChatMessage message in _messages) {
+      message.animationController.dispose();
+    }
+
+    super.dispose();
   }
 }
--- lib/views/users_view.dart
+++ lib/views/users_view.dart
@@ -1,6 +1,6 @@
 import 'package:chat_app/models/user.dart';
 import 'package:flutter/material.dart';
-import 'package:pull_to_refresh_notification/pull_to_refresh_notification.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class UsersPage extends StatefulWidget {
   const UsersPage({required Key key}) : super(key: key);
@@ -10,9 +10,6 @@ class UsersPage extends StatefulWidget {
 }
 
 class _UsersPageState extends State<UsersPage> {
-  RefreshController _refreshController =
-      RefreshController(initialRefresh: false);
-
   final users = [
     User(uid: '1', name: 'Maria', email: 'test1@test.com', online: true),
     User(uid: '2', name: 'Jose', email: 'test2@test.com', online: false),
@@ -43,9 +40,9 @@ class _UsersPageState extends State<UsersPage> {
           )
         ],
       ),
-      body: PullToRefreshNotification(
+      body: SmartRefresher(
         onRefresh: _uploadUsers,
-        controller: _refreshController,
+        controller: RefreshController(),
         child: ListView.separated(
           physics: const BouncingScrollPhysics(),
           itemBuilder: (_, i) => _userListTile(users[i]),
@@ -57,26 +54,27 @@ class _UsersPageState extends State<UsersPage> {
   }
 
   Future<bool> _uploadUsers() async {
-    await Future.delayed(const Duration(milliseconds: 1000));
-    _refreshController.refreshCompleted();
+    await Future.delayed(const Duration(milliseconds: 500));
+    RefreshController().refreshCompleted();
     return true; // Indicate that the refresh operation was successful
   }
 
   ListTile _userListTile(User user) {
     return ListTile(
-        title: Text(user.name),
-        subtitle: Text(user.email),
-        leading: CircleAvatar(
-          backgroundColor: Colors.blue[200],
-          child: Text(user.name.substring(0, 2)),
+      title: Text(user.name),
+      subtitle: Text(user.email),
+      leading: CircleAvatar(
+        backgroundColor: Colors.blue[200],
+        child: Text(user.name.substring(0, 2)),
+      ),
+      trailing: Container(
+        width: 10,
+        height: 10,
+        decoration: BoxDecoration(
+          color: user.online ? Colors.green[300] : Colors.red[600],
+          borderRadius: BorderRadius.circular(100),
         ),
-        trailing: Container(
-          width: 10,
-          height: 10,
-          decoration: BoxDecoration(
-            color: user.online ? Colors.green[300] : Colors.red[600],
-            borderRadius: BorderRadius.circular(100),
-          ),
-        ));
+      ),
+    );
   }
 }
